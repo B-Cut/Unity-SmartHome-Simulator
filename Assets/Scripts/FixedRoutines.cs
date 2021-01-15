e36069c4e8899bef9111e1bd10bc8e3264af0abc
B-Cut <kaylagbst@hotmail.com>
2021-01-15 11:48:33 -0300

    Adiciona arquivos que não foram faltando do ultimo commit

---
+++ Assets/Scripts/FixedRoutines.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedRoutines : MonoBehaviour
+{
+    //Numero total de atividades diarias
+    public static int numberOfActivities = new int();
+
+    public TimeManagement timeManagement;
+    public Pessoa pessoa;
+    //Cria novo array com todas as atividades que o usuário realizara pelo dia
+    public string[] activityTimes = new string[numberOfActivities];
+    public string[] activities = new string[numberOfActivities];
+    public string nextActivity = "";
+    public int nextActivityTime = new int();
+
+    public int counter = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextActivityTime = ActivityTimeToSecond(activityTimes[counter]);
+        nextActivity = activities[counter];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timeManagement.getTime() == nextActivityTime){
+            pessoa.pushToQueue(nextActivity);
+            counter++;
+            if(counter == activities.Length){
+                counter = 0;
+            }
+            
+            nextActivityTime = ActivityTimeToSecond(activityTimes[counter]);
+            nextActivity = activities[counter];
+        }
+    }
+
+    private int ActivityTimeToSecond(string time){
+        string[] splitTime = time.Split(':');
+        int hour = int.Parse(splitTime[0]);
+        int minute = int.Parse(splitTime[1]);
+        return (int) timeManagement.ToSecond(hour, minute);
+    }
+}
---
+++ Assets/Scripts/IntervalRoutine.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntervalRoutine : MonoBehaviour
+{
+    public TimeManagement timeManagement;
+    public Pessoa pessoa;
+
+    public int mealInterval = 3;//hours
+    public int bathroomInterval = 4;//hours
+    public bool emptyQueue = false;
+    private bool bathOnQueue = false;
+    private string nextActivity = "";
+    public bool isRelaxing = false;
+
+    public int cookingTimeHour = 12;
+    public int cookingTimeMinute = 0;
+
+    public int bathroomTimeHour = 8;
+    public int bathroomTimeMinute = 0;
+
+    public int bathTimeHour = 14;
+    public int bathTimeMinute = 0;
+
+
+    public int sleepTimeHour = 22;
+    public int sleepTimeMinute = 0;
+
+    public int wakeUpTimeHour = 8;
+    public int wakeUpTimeMinute = 0;
+    public bool sleepInQueue = false;
+    private int today = new int();
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timeManagement.getTime() >= timeManagement.ToSecond(cookingTimeHour, cookingTimeMinute)){
+                pessoa.pushToQueue("cook");
+                cookingTimeHour += 3;
+                //if(cookingTimeHour >= 24) cookingTimeHour -= 24;
+            }
+
+            if(timeManagement.getTime() >= timeManagement.ToSecond(bathroomTimeHour, bathroomTimeMinute)){
+                pessoa.pushToQueue("useBathroom");
+                bathroomTimeHour += bathroomInterval;
+                //if(bathroomTimeHour >= 24) bathroomTimeHour -= 24;
+            }
+
+            if(timeManagement.getTime() >= timeManagement.ToSecond(bathTimeHour, bathroomTimeMinute) && !bathOnQueue){
+                pessoa.pushToQueue("takeBath");
+                bathOnQueue = true;
+            }
+
+            if(timeManagement.getTime() >= timeManagement.ToSecond(sleepTimeHour, sleepTimeMinute) && !sleepInQueue){
+                pessoa.pushToQueue("sleep");
+                sleepInQueue = true;
+            }
+            //Novo Dia
+            if(timeManagement.getDate() > today){
+                if(cookingTimeHour >= 24) cookingTimeHour = wakeUpTimeHour;
+                if(bathroomTimeHour >= 24) bathroomTimeHour = wakeUpTimeHour;
+                today = timeManagement.getDate();
+            }
+
+            if(emptyQueue && !isRelaxing){ 
+                pessoa.pushToQueue("relax");
+                isRelaxing = true;
+            }
+            if(pessoa.getActivityCount() > 0){
+                emptyQueue = false;
+            }  
+    }
+}
